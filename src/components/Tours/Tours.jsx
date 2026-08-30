527f99fc3b94637a2c2fcc7de21cea0f14cb1f0f
faiezv <faiezviljoen@gmail.com>
2026-08-30 17:21:41 +0200

    Minor update: OnLoad Page Jump Tours caused by ToursBrowser Error v2

--- src/components/Tours/Tours.jsx
+++ src/components/Tours/Tours.jsx
@@ -9,13 +9,13 @@ const Tours = () => {
 
   return (
     <div className="relative w-full overflow-x-hidden bg-white text-black">
-      {/* <section className="relative z-10 max-w-5xl mx-auto">
+      <section className="relative z-10 max-w-5xl mx-auto">
         <ToursBanner />
-      </section> */}
-
-      <section className="relative z-10  overflow-visibl bg-blue-200">
-        <ToursBrowser />
       </section>
+
+      {/* <section className="relative z-10  overflow-visibl bg-blue-200">
+        <ToursBrowser />
+      </section> */}
     </div>
   );
 };
